ea3cf3495255dcb0e5491e4d6657e8d7bb42b4cb
Mohanlal Gopu <mohanlal.gopu@syncfusion.com>
2021-01-18 09:08:23 +0530

    Upload new version.

---
+++ SimpleSample/SimpleSample/ViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace SimpleSample
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+
+        public List<Model> Items { get; set; }
+
+        public ViewModel()
+        {
+            Items = new List<Model>()
+            {
+                new Model(){Item = "C#" , IsChecked = true},
+                new Model(){Item = "C++" , IsChecked = false},
+                new Model(){Item = "C" , IsChecked = false},
+                new Model(){Item = "JAVA" , IsChecked = false},
+
+            };
+
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
